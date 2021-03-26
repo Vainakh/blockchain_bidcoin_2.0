1c60f5dbb9a6ed269e20ee084a2ff49f96c1c28d
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-03-25 21:27:43 -0700

    Create api foundation

--- dev/api.js
+++ dev/api.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send("Hello World!")
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.get('/blockchain', (req, res) => {
+  
+});
+
+app.post('/transaction', (req, res) => {
+  console.log(req.body);
+  res.send(`The amount of the transaction is ${req.body.amount} bidcoins.`)
+});
+
+app.get('/mine', (req, res) => {
+  
 });
 
 app.listen(3000, () => {
-  console.log('Server is up and running...')
+  console.log('Server is up and running on port 3000...')
 });
